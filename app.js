a58895ae63d5b15351dfd1810d8a771ce2c215ec
RJay Sharma <sharma.rajan3097@gmail.com>
2021-12-20 11:24:26 +0530

    mvc imnplementation

--- app.js
+++ app.js
@@ -2,22 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const { engine } = require('express-handlebars');
+const errorController = require('./controllers/error');
 
 const app = express();
-
-
-// Handlebars View Engine, we need to register view engine manually
-// app.engine('hbs', engine({
-//   defaultLayout: 'main-layout',
-//   layoutsDir: 'views/layouts/',
-//   extname: 'hbs'
-// }));
-// app.set('view engine', 'hbs');
-
-// Pug View Engine
-// app.set('view engine', 'pug');
-
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -25,16 +12,13 @@ app.set('views', './views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  res.status(404).render('404', { pageTitle: 'Page Not Found!!' })
-});
+app.use(errorController.get404);
 
 app.listen(3000);
