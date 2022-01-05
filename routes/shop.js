b4e9901ea0c706890884ffb2a6ef5e784dc94976
RJay Sharma <sharma.rajan3097@gmail.com>
2022-01-05 14:03:10 +0530

    implemented sequelize ORM

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 
-const shopController = require('../controllers/shop');
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
@@ -17,6 +17,8 @@ router.get("/cart", shopController.getCart);
 router.post("/cart", shopController.postCart);
 
 router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+
+router.post("/create-order", shopController.postOrder);
 
 router.get("/orders", shopController.getOrders);
 
