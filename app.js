f5a67e5f0e02c954d3c22f5e551925d8e53bcad9
RJay Sharma <sharma.rajan3097@gmail.com>
2022-02-21 14:31:50 +0530

    implemented paginations

--- app.js
+++ app.js
@@ -105,6 +105,7 @@ app.use("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
+  console.log(error);
   // res.redirect("/500");
   res.status(500).render("500", {
     pageTitle: "Internal Server Error!",
