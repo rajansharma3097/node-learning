c9fde13dd04444b0e309a41ec720bf8bbf866b3e
RJay Sharma <sharma.rajan3097@gmail.com>
2022-02-22 12:56:02 +0530

    implemented stripe payment integration

--- routes/shop.js
+++ routes/shop.js
@@ -18,12 +18,16 @@ router.post("/cart", isAuth, shopController.postCart);
 
 router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
 
-router.post("/create-order", isAuth, shopController.postOrder);
+router.get("/checkout", shopController.getCheckout);
+
+router.get("/checkout/success", shopController.getCheckoutSuccess);
+
+router.get("/checkout/cancel", shopController.getCheckout);
+
+// router.post("/create-order", isAuth, shopController.postOrder);
 
 router.get("/orders", isAuth, shopController.getOrders);
 
 router.get("/orders/:orderId", isAuth, shopController.getInvoice);
-
-// router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
